15476f6d2bb6d961d67f0af69126e20453de99c9
Jason Jean <jasonjean1993@gmail.com>
2023-09-22 16:50:11 -0400

    Initial commit

---
+++ e2e/src/e2e/app.cy.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('nxconf-demo2023', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome nxconf-demo2023');
+  });
+});
